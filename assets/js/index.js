d96cf2378c3279c89fd54f1ea812459da35df5b5
diahevyka <diah.hevyka@gmail.com>
2017-08-28 17:19:11 +0700

    12638
    +modals

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+$('#daftarworkshop').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+
+$('#daftarSG').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+
+$('#daftarRG').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
